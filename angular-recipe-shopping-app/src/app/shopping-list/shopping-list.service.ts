7c3d077c7c877c186bed751bf2872e6e8d2547a4
lxieyang <lxieyang@umich.edu>
2017-12-14 19:34:35 -0500

    use observables to improve final project

--- angular-recipe-shopping-app/src/app/shopping-list/shopping-list.service.ts
+++ angular-recipe-shopping-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
-import { EventEmitter } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
+import { Subject } from 'rxjs/Subject';
 
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Banana', 50),
@@ -16,7 +16,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
@@ -24,6 +24,6 @@ export class ShoppingListService {
     //   this.addIngredient(ing);
     // }
     this.ingredients.push(...ingredients);  // ES6 spread operator to spread elements in an array to be pushed
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
