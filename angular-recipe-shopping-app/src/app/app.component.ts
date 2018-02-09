1e43b4caea50a02911c19f0b3d3f9711dc06af4a
lxieyang <lxieyang@umich.edu>
2018-02-08 20:05:04 -0500

    setup authentication

--- angular-recipe-shopping-app/src/app/app.component.ts
+++ angular-recipe-shopping-app/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { apiKey, authDomain } from '../config.app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   loadedFeature: string = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: apiKey,
+      authDomain: authDomain
+    });
+  }
   
   onNavigate(feature: string) {
     this.loadedFeature = feature;
---
+++ angular-recipe-shopping-app/src/app/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+export class AuthService {
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {}).catch((error) => console.log(error));
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+}
